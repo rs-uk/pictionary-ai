2e644b944b7286b35859289f6626e15bd1e3453b
Loic Regne <loic.regne@gmail.com>
2024-03-08 11:23:01 +0000

    cleaning up

--- pictionary_ai/params.py
+++ pictionary_ai/params.py
@@ -3,9 +3,8 @@ import os
 #####################  CONSTANTS  #####################
 MAX_LENGTH = 150 # Max number of points we keep in a drawing for training and inference
 
-LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), 'code', 'rs-uk', 'pictionary-ai', 'raw_data')
-
-
+# Set LOCAL_DATA_PATH to the "raw_data" folder:
+LOCAL_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "raw_data")
 
 
 #####################  VARIABLES  #####################
