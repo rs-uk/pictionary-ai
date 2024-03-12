36faa5c882aa8e5cc8dec3c40f41846dd52df340
Gregory Taylor <gregorytaylor902@gmail.com>
2024-03-12 09:43:49 +0000

    homepage changes added

--- pictionary_ai/pictionary_website/Homepage.py
+++ pictionary_ai/pictionary_website/Homepage.py
@@ -2,31 +2,35 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-
 import streamlit as st
 import time
 
 from streamlit_extras.add_vertical_space import add_vertical_space
 
-image_path = "/Users/gregorytaylor/code/pictionary-ai/pictionary_ai.jpg"
+image_path = "/Users/gregorytaylor/code/pictionary-ai/raw_data/pictionary_ai.jpg" #this needs to be amended
 
-st.set_page_config(page_icon=":shark:")
+# st.set_page_config(page_icon=":pencil:") #this sets the icon for the tab
 
-
-
-backgroundColor = "#F0F0F0"
-
-
-
-
-st.markdown("<h1 style='text-align: center;'>Pictionary AI</h1>", unsafe_allow_html=True)
+st.set_page_config(
+            page_title="Homepage", # => Quick reference - Streamlit
+            page_icon=":pencil:",
+            layout="centered", # wide
+            initial_sidebar_state="auto")
 
 
 
 
-st.image(image_path, use_column_width=True)
 
-st.markdown("<h1 style='text-align: center; font-weight: normal; font-size: 22px;'>Welcome to Pictionary AI, the transformative new website that can predict your drawings!</h1>", unsafe_allow_html=True)
+
+
+st.markdown("<h1 style='text-align: center;'>Pictionary AI</h1>", unsafe_allow_html=True) #centralised title
+
+
+
+
+st.image(image_path, use_column_width=True) #pictionary ai logo image
+
+st.markdown("<h1 style='text-align: center; font-weight: normal;font-size: 22px;'>Welcome to Pictionary AI, the transformative new website that can predict your drawings!</h1>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; font-weight: normal; font-size: 14px;'>You have 20 seconds to draw whatever image we give to you. After that we will test it to see if your drawing is good     enough for the AI model to match it.</h1>"
 , unsafe_allow_html=True)
@@ -35,14 +39,14 @@ st.markdown("<h1 style='text-align: center; font-weight: normal; font-size: 14px
 
 
 
-image1 = '/Users/gregorytaylor/code/pictionary-ai/raw_data/preview.jpg'
-add_vertical_space(3)
+image1 = '/Users/gregorytaylor/code/pictionary-ai/raw_data/preview.jpg' #this also needs to be changed
+add_vertical_space(3) #formatting
 
 col1, col2, col3 = st.columns(3)
 
-# Add content to the columns
-col1.write("       ")  # Empty column
-col2.link_button("   Click here to play", "http://localhost:8502/Game")  # Link button in the middle
+
+col1.write("")  # Empty column
+col2.link_button(":blue[Click here to play the game]", "http://localhost:8501/Game")  # Link button in the middle
 col3.write("")  # Empty column
 
 
@@ -50,12 +54,9 @@ col3.write("")  # Empty column
 
 
 
-add_vertical_space(3)
+add_vertical_space(3) #formatting
 st.markdown("<h1 style='text-align: center; font-weight: normal; font-size: 18px;'>Below are some examples of the sorts of images you may be asked to draw.</h1>", unsafe_allow_html=True)
 
-
-st.image(image1, width=710)
-
-
-
 image1 = '/Users/gregorytaylor/code/pictionary-ai/raw_data/preview.jpg'
+
+st.image(image1, width=710) #bottom of page image
